28f740302aa012dd1f0319f03568dceb5f86c74a
EhabMagdyy <ehabmagdy0044@gmail.com>
2026-01-04 00:01:29 +0200

    EEPROM Write/Read Runnable

--- include/interface/HAL/eeprom.h
+++ include/interface/HAL/eeprom.h
@@ -8,11 +8,19 @@ typedef struct{
     uint8_t deviceAddress;
 } EEPROM_Config_t;
 
+typedef enum{
+    EEPROM_READ_SEND_ADDR = 0,
+    EEPROM_READ_DATA,
+    EEPROM_IDLE
+} EEPROM_State_t;
+
 STD_ReturnType EEPROM_Init(const EEPROM_Config_t* config);
 STD_ReturnType EEPROM_DeInit(const EEPROM_Config_t* config);
 STD_ReturnType EEPROM_WriteData(const EEPROM_Config_t* config, uint16_t address, uint8_t* data, uint8_t length);
 STD_ReturnType EEPROM_ReadData(const EEPROM_Config_t* config, uint16_t address, uint8_t* data, uint8_t length);
-STD_ReturnType EEPROM_WritePage(const EEPROM_Config_t* config, uint16_t address, const I2C_Buffer_t* buffer);
-STD_ReturnType EEPROM_ReadPage(const EEPROM_Config_t* config, uint16_t address, const I2C_Buffer_t* buffer);
+STD_ReturnType EEPROM_WritePage(const EEPROM_Config_t* config, uint8_t address, uint8_t* buffer);
+STD_ReturnType EEPROM_ReadPage(const EEPROM_Config_t* config, uint8_t address, uint8_t* buffer);
+
+STD_ReturnType EEPROM_WriteVerify(const EEPROM_Config_t* config, uint8_t address, uint8_t* txData, uint8_t* rxData, uint8_t length);
 
 #endif // EEPROM_H
--- src/HAL/interface/eeprom.c
+++ src/HAL/interface/eeprom.c
@@ -1,12 +1,13 @@
 #include "interface/HAL/eeprom.h"
-#include "interface/Core/systick.h"
+#include "OS/scheduler.h"
 #include <string.h>
 
 uint8_t i2cData[17];    // 1 byte for address + 16 bytes for data
 I2C_Buffer_t i2cBuffer; // Buffer for I2C operations
 I2C_Buffer_t i2cAddr; // Buffer for I2C operations
-uint8_t addr;
+uint8_t blockAddress;
 
+volatile EEPROM_State_t eepromState = EEPROM_IDLE;
 
 STD_ReturnType EEPROM_Init(const EEPROM_Config_t* config){
     STD_ReturnType ret = STD_SUCCESS;
@@ -35,6 +36,9 @@ STD_ReturnType EEPROM_WriteData(const EEPROM_Config_t* config, uint16_t address,
     if(config == NULL || data == NULL || length == 0 || length > 16){
         ret = STD_ERROR;
     }
+    else if(I2C_GetState(config->i2cConfig->i2cNumber) != I2C_STATE_IDLE){
+        ret = STD_BUSY;
+    }
     else{
         i2cData[0] = address & 0xFF;    // Set the memory address to write to
         memcpy(i2cData+1, data, length);
@@ -52,40 +56,88 @@ STD_ReturnType EEPROM_ReadData(const EEPROM_Config_t* config, uint16_t address, 
     if(config == NULL || data == NULL || length == 0 || length > 16){
         ret = STD_ERROR;
     }
+    else if(I2C_GetState(config->i2cConfig->i2cNumber) != I2C_STATE_IDLE){
+        ret = STD_BUSY;
+    }
     else{
-        addr = address & 0xFF; // Set the memory address to read from
-        // Prepare I2C buffer
-        i2cAddr.data = &addr;
-        i2cAddr.length = 1;
-        // Send memory address
-        ret = I2C_Master_TransmitIT(config->i2cConfig, config->deviceAddress | (address >> 8), &i2cAddr);
-        SYSTICK_DelayMS(10);
-        i2cBuffer.length = length;
-        i2cBuffer.data = data;
-        // Read data from EEPROM
-        ret = I2C_Master_ReceiveIT(config->i2cConfig, config->deviceAddress | (address >> 8), &i2cBuffer);
+        if(eepromState == EEPROM_IDLE){
+            blockAddress = address & 0xFF; // Set the memory address to read from
+            // Prepare I2C buffer
+            i2cAddr.data = &blockAddress;
+            i2cAddr.length = 1;
+            i2cBuffer.length = length;
+            i2cBuffer.data = data;
+            // Send memory address
+            ret = I2C_Master_TransmitIT(config->i2cConfig, config->deviceAddress | (address >> 8), &i2cAddr);
+            eepromState = EEPROM_READ_DATA;
+        }
+        else if(eepromState == EEPROM_READ_DATA){
+            ret = I2C_Master_ReceiveIT(config->i2cConfig, config->deviceAddress | (address >> 8), &i2cBuffer);
+            eepromState = EEPROM_IDLE;
+        }
+        else{
+            ret = STD_BUSY;
+        }
     }
     return ret;
 }
 
-STD_ReturnType EEPROM_WritePage(const EEPROM_Config_t* config, uint16_t address, const I2C_Buffer_t* buffer){
+STD_ReturnType EEPROM_WritePage(const EEPROM_Config_t* config, uint8_t addressOffset, uint8_t* data){
     STD_ReturnType ret = STD_SUCCESS;
-    if(config == NULL || buffer == NULL || buffer->data == NULL || buffer->length == 0){
+    if(config == NULL || data == NULL){
         ret = STD_ERROR;
     }
     else{
-        
+        EEPROM_WriteData(config, addressOffset*16, data, 16);
     }
     return ret;
 }
 
-STD_ReturnType EEPROM_ReadPage(const EEPROM_Config_t* config, uint16_t address, const I2C_Buffer_t* buffer){
+STD_ReturnType EEPROM_ReadPage(const EEPROM_Config_t* config, uint8_t addressOffset, uint8_t* data){
     STD_ReturnType ret = STD_SUCCESS;
-    if(config == NULL || buffer == NULL || buffer->data == NULL || buffer->length == 0){
+    if(config == NULL || data == NULL){
         ret = STD_ERROR;
     }
     else{
-        
+        EEPROM_ReadData(config, addressOffset*16, data, 16);
+    }
+    return ret;
+}
+
+STD_ReturnType EEPROM_WriteVerify(const EEPROM_Config_t* config, uint8_t address, uint8_t* txData, uint8_t* rxData, uint8_t length){
+    STD_ReturnType ret = STD_SUCCESS;
+    if(config == NULL || txData == NULL || rxData == NULL){
+        ret = STD_ERROR;
+    }
+    else{
+        if(eepromState == EEPROM_IDLE){
+            i2cData[0] = address & 0xFF;    // Set the memory address to write to
+            memcpy(i2cData+1, txData, length);
+            // Prepare I2C buffer
+            i2cBuffer.data = i2cData;
+            i2cBuffer.length = length + 1;
+            // Transmit data to EEPROM
+            ret = I2C_Master_TransmitIT(config->i2cConfig, config->deviceAddress | (address >> 8), &i2cBuffer);
+            eepromState = EEPROM_READ_SEND_ADDR;
+        }
+        else if(eepromState == EEPROM_READ_SEND_ADDR){
+            blockAddress = address & 0xFF; // Set the memory address to read from
+            // Prepare I2C buffer
+            i2cAddr.data = &blockAddress;
+            i2cAddr.length = 1;
+            i2cBuffer.length = length;
+            i2cBuffer.data = rxData;
+            // Send memory address
+            ret = I2C_Master_TransmitIT(config->i2cConfig, config->deviceAddress | (address >> 8), &i2cAddr);
+            eepromState = EEPROM_READ_DATA;
+        }
+        else if(eepromState == EEPROM_READ_DATA){
+            ret = I2C_Master_ReceiveIT(config->i2cConfig, config->deviceAddress | (address >> 8), &i2cBuffer);
+            eepromState = EEPROM_IDLE;
+        }
+        else{
+            ret = STD_BUSY;
+        }
     }
     return ret;
 }
