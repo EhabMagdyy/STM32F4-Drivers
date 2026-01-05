3479b398b17f9a363eb44d584819744f6282d3cc
EhabMagdyy <ehabmagdy0044@gmail.com>
2026-01-05 17:45:59 +0200

    Encabsulated EEPROM Runnable

--- include/interface/HAL/eeprom.h
+++ include/interface/HAL/eeprom.h
@@ -3,9 +3,12 @@
 
 #include "interface/MCAL/i2c.h"
 
+typedef void (*EEPROM_Callback_t)(uint8_t);
+
 typedef struct{
     I2C_Config_t* i2cConfig;
     uint8_t deviceAddress;
+    EEPROM_Callback_t operationCompleteCallback;
 } EEPROM_Config_t;
 
 typedef enum{
@@ -16,15 +19,10 @@ typedef enum{
 
 STD_ReturnType EEPROM_Init(const EEPROM_Config_t* config);
 STD_ReturnType EEPROM_DeInit(const EEPROM_Config_t* config);
-// ONE TIME
 STD_ReturnType EEPROM_WriteData(const EEPROM_Config_t* config, uint16_t address, uint8_t* data, uint8_t length);
-// TWO TIME
 STD_ReturnType EEPROM_ReadData(const EEPROM_Config_t* config, uint16_t address, uint8_t* data, uint8_t length);
-// ONE TIME
 STD_ReturnType EEPROM_WritePage(const EEPROM_Config_t* config, uint8_t pageOffset, uint8_t* buffer);
-// TWO TIMES
 STD_ReturnType EEPROM_ReadPage(const EEPROM_Config_t* config, uint8_t pageOffset, uint8_t* buffer);
-// THREE TIMES
 STD_ReturnType EEPROM_WriteVerify(const EEPROM_Config_t* config, uint16_t address, uint8_t* txData, uint8_t* rxData, uint8_t length);
 
 #endif // EEPROM_H
--- src/HAL/interface/eeprom.c
+++ src/HAL/interface/eeprom.c
@@ -2,12 +2,84 @@
 #include "OS/scheduler.h"
 #include <string.h>
 
-uint8_t i2cData[17];    // 1 byte for address + 16 bytes for data
+uint8_t i2cData[17];    // 1 byte for address + 16 bytes for data / 16 bytes for data + 1 Dont care
 I2C_Buffer_t i2cBuffer; // Buffer for I2C operations
-I2C_Buffer_t i2cAddr; // Buffer for I2C operations
-uint8_t blockAddress;
+I2C_Buffer_t i2cAddr;   // Buffer for I2C operations
+uint8_t blockAddress;   // Address of the memory block to read/write
 
 volatile EEPROM_State_t eepromState = EEPROM_IDLE;
+EEPROM_Callback_t myCallback = NULL;
+
+// For Runnable
+typedef enum{
+    EEPROM_OP_READ = 0,
+    EEPROM_OP_WRITE_VERIFY,
+    EEPROM_OP_IDLE
+} EEPROM_OP_t;
+EEPROM_OP_t currentOperation = EEPROM_OP_IDLE;
+EEPROM_Config_t* myConfig = NULL;
+uint16_t myAddress = 0;
+uint8_t pageOffset = 0;
+uint8_t myDataLength = 0;
+uint8_t* myWriteData = NULL;
+uint8_t* myReadData = NULL;
+uint8_t counter = 0;
+
+void EEPROM_Runnable(void* arg){
+    static STD_ReturnType ret = STD_SUCCESS;
+    switch(currentOperation){
+        case EEPROM_OP_READ:
+            if(counter == 0){
+                counter++;
+            }
+            else if(counter == 1){
+                ret = EEPROM_ReadData(myConfig, myAddress, myReadData, myDataLength);
+                if(myCallback)
+                    myCallback(ret);
+                // Reset operation
+                currentOperation = EEPROM_OP_IDLE;
+            }
+            break;
+
+        case EEPROM_OP_WRITE_VERIFY:
+            if(counter == 0){
+                counter++;
+            }
+            else if(counter < 3){
+                ret = EEPROM_WriteVerify(myConfig, myAddress, myWriteData, myReadData, myDataLength);
+                if(ret == STD_SUCCESS)
+                    counter++;
+            }
+            else if(counter == 3){
+                uint8_t match = 1;
+                // verify that myWriteData matches myReadData
+                for(uint8_t i = 0; i < myDataLength; i++){
+                    if(myWriteData[i] != myReadData[i]){
+                        match = 0;
+                    }
+                }
+                if(myCallback)
+                    myCallback(match);
+                // Reset operation
+                currentOperation = EEPROM_OP_IDLE;
+            }
+            else{
+                // Nothing
+            }
+            break;
+        
+        default:
+            // Do nothing
+            break;
+    }
+}
+
+static Runnable__t eepromRunnable = {
+    .callback = EEPROM_Runnable,
+    .Periodicity = 10,
+    .FirstDelay = 100,
+    .arg = NULL
+};
 
 STD_ReturnType EEPROM_Init(const EEPROM_Config_t* config){
     STD_ReturnType ret = STD_SUCCESS;
@@ -16,6 +88,8 @@ STD_ReturnType EEPROM_Init(const EEPROM_Config_t* config){
     }
     else{
         ret = I2C_Init(config->i2cConfig);
+        myCallback = config->operationCompleteCallback;
+        ret = Scheduler_RegisterRunnable(&eepromRunnable);
     }
     return ret;
 }
@@ -61,6 +135,16 @@ STD_ReturnType EEPROM_ReadData(const EEPROM_Config_t* config, uint16_t address, 
     }
     else{
         if(eepromState == EEPROM_IDLE){
+            // Store Parameters for Runnable
+            myConfig = (EEPROM_Config_t*)config;
+            myAddress = address;
+            myWriteData = NULL;
+            myReadData = data;
+            myDataLength = length;
+            counter = 0;
+            currentOperation = EEPROM_OP_READ;
+
+            // Start Read Operation - Send Address Phase
             blockAddress = address & 0xFF; // Set the memory address to read from
             // Prepare I2C buffer
             i2cAddr.data = &blockAddress;
@@ -111,6 +195,16 @@ STD_ReturnType EEPROM_WriteVerify(const EEPROM_Config_t* config, uint16_t addres
     }
     else{
         if(eepromState == EEPROM_IDLE){
+            // Store parameters for Runnable
+            myConfig = (EEPROM_Config_t*)config;
+            myAddress = address;
+            myWriteData = txData;
+            myReadData = rxData;
+            myDataLength = length;
+            counter = 0;
+            currentOperation = EEPROM_OP_WRITE_VERIFY;
+
+            // Start Write Operation
             i2cData[0] = address & 0xFF;    // Set the memory address to write to
             memcpy(i2cData+1, txData, length);
             // Prepare I2C buffer
@@ -121,6 +215,7 @@ STD_ReturnType EEPROM_WriteVerify(const EEPROM_Config_t* config, uint16_t addres
             eepromState = EEPROM_READ_SEND_ADDR;
         }
         else if(eepromState == EEPROM_READ_SEND_ADDR){
+            // Read Operation - Send Address Phase
             blockAddress = address & 0xFF; // Set the memory address to read from
             // Prepare I2C buffer
             i2cAddr.data = &blockAddress;
@@ -130,6 +225,7 @@ STD_ReturnType EEPROM_WriteVerify(const EEPROM_Config_t* config, uint16_t addres
             eepromState = EEPROM_READ_DATA;
         }
         else if(eepromState == EEPROM_READ_DATA){
+            // Read Operation - Data Phase
             i2cBuffer.length = length;
             i2cBuffer.data = rxData;
             // Receive data from EEPROM
